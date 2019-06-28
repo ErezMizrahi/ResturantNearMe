b60c922685e2ca397cf59192030d75f685432d19
hackeru <hackeru@hackerus-Mac.local>
2019-06-28 15:16:39 -0700

    Initial Commit

---
+++ Yummy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yummy
+//
+//  Created by hackeru on 28/06/2019.
+//  Copyright © 2019 erez8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
